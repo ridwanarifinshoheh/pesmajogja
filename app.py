83acfd425f09d26f306d9afab45a9b51ef540a37
ridwanarifinshoheh <ridwanarifinshoheh01@gmail.com>
2026-05-06 22:29:16 +0700

    update

--- app.py
+++ app.py
@@ -10,18 +10,3 @@ def home():
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route('/css/<path:filename>')
-def css_files(filename):
-    return send_from_directory('css', filename)
-
-@app.route('/js/<path:filename>')
-def js_files(filename):
-    return send_from_directory('js', filename)
-
-@app.route('/images/<path:filename>')
-def images_files(filename):
-    return send_from_directory('images', filename)
-
-@app.route('/fonts/<path:filename>')
-def fonts_files(filename):
-    return send_from_directory('fonts', filename)
