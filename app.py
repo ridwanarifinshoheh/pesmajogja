ee67c41b5c562b4d20fcd35028e8ff2d6099b711
ridwanarifinshoheh <ridwanarifinshoheh01@gmail.com>
2026-05-07 17:19:45 +0700

    new modern landing page

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# ================= HOME =================
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+# ================= RUN =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
